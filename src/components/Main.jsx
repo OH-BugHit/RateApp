113af03bdcb31f0033d4058dd294e8be52a0556d
Olli Hilke <hilkolli@student.uef.fi>
2023-12-21 16:16:20 +0200

    ennen 10.4

---
+++ src/components/Main.jsx
@@ -0,0 +1,25 @@
+import Constants from "expo-constants";
+import { StyleSheet, View } from "react-native";
+import RepositoryList from "./RepositoryList";
+import Text from "./Text";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text fontWeight="bold" fontSize="heading" textAlign="center">
+        Rate Repository Application
+      </Text>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default Main;
